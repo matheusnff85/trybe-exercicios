77b14ff5c3630dd98af8c5cfeab2e14f68c37722
Matheus Marinho <math.marinho154@gmail.com>
2022-02-09 16:19:26 -0300

    exercicios pre aula

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicios5.1/script.js
@@ -0,0 +1,10 @@
+document.querySelector("h1").style.backgroundColor = "green";
+document.querySelector("section").style.backgroundColor = "orange";
+document.querySelectorAll("section")[1].style.backgroundColor = "yellow";
+
+document.querySelectorAll("section h3")[0].style.backgroundColor = "violet";
+document.querySelectorAll("section h3")[1].style.backgroundColor = "violet";
+document.querySelectorAll("section h3")[2].style.backgroundColor = "black";
+document.querySelectorAll("section h3")[3].style.backgroundColor = "black";
+
+document.getElementById("footer-container").style.backgroundColor = "#006400";
