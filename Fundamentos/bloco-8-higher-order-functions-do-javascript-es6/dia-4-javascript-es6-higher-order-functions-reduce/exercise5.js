5f324fb00e0c2b42f8d4f3d987f32ba7df710d44
Matheus Marinho <math.marinho154@gmail.com>
2022-03-14 19:03:21 -0300

    exercicios dia 8.4

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise5.js
@@ -0,0 +1,15 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA(array) {
+  return array.reduce((acc, curr) =>
+     acc + curr.split('').reduce((acumulator, current) => {
+        if (current === 'a' || current === 'A') return acumulator + 1;
+        return acumulator;
+     }, 0), 0);
+}
+
+console.log(containsA(names))
