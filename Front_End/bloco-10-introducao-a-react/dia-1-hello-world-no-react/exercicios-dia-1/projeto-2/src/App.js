a8dc31d38bb805ba5fa063815d73008c10c8e388
Matheus Marinho <math.marinho154@gmail.com>
2022-04-04 19:29:09 -0300

    Dia 10.1

---
+++ Front_End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicios-dia-1/projeto-2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import './exercise.css'
+
+class App extends React.Component {
+  render() {
+    return (<><Header/><Content/><Footer/></>);
+  }
+}
+
+export default App;
